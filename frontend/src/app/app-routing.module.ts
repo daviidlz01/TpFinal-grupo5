fd356811fcd998df3181e27781a92f6fb5c5d2da
Tiago Brizuela <tiagobrizuela18@gmail.com>
2022-06-24 15:37:31 -0300

    Agregando routing basico

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmpleadoComponent } from './components/empleado/empleado.component';
+import { HomeComponent } from './components/home/home.component';
+import { ReunionComponent } from './components/reunion/reunion.component';
 import { Empleado } from './models/empleado';
 const routes: Routes = [
+  {path:'home', component:HomeComponent},
   {path: 'empleado', component: EmpleadoComponent},
+  {path: 'reunion', component: ReunionComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
   
   
   //pruebas
