1006abea35de61599136b529b10cf267ed6da965
marcioRLopez <50425634+marcioRLopez@users.noreply.github.com>
2022-06-24 12:33:50 -0300

    codificacion de GET en empleado.service

--- frontend/src/app/services/empleado.service.ts
+++ frontend/src/app/services/empleado.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Empleado } from '../models/empleado';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Oficina } from '../models/oficina';
+import { Recurso } from '../models/recurso';
+import { Reunion } from '../models/reunion';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+//urls para conectarse a la base de datos
+
+  private urlBase ="http://localhost:3000/api/reunion";
+  private urlBase1 ="http://localhost:3000/api/empleado";
+  private urlBase2 ="http://localhost:3000/api/recursos";
+
+  constructor(private _http:HttpClient) { }
+//traigo todas las reuniones
+
+  getReunion():Observable<any>{
+    const Options ={
+      method: "GET",
+      headers: new HttpHeaders({}),
+      params:new HttpParams({})
+    }
+    return this._http.get(this.urlBase + "/mostrar",Options)
+  }
+
+  getRecurso():Observable<any>{
+    const Options ={
+      method: "GET",
+      headers: new HttpHeaders({}),
+      params:new HttpParams({})
+    }
+    return this._http.get(this.urlBase + "/mostrar",Options)
+  }
+  getEmpleado():Observable<any>{
+    const Options ={
+      method: "GET",
+      headers: new HttpHeaders({}),
+      params:new HttpParams({})
+    }
+    return this._http.get(this.urlBase + "/mostrar",Options)
+  }
+
+
+
+
+
+
+
+
+
 }
